6db6f32c5ecf4cae8f45bcab7d54294328a27ab2
ksenginew <ksengine.github@gmail.com>
2023-09-01 21:04:47 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ const TYPE = Symbol()
 export function render(comp, target) {
     if (comp[TYPE] !== 0) throw Error()
     if (typeof comp.name === "function") return render(comp.name(comp.props), target)
+    const { children, ...props } = comp.props
     let el = document.createElement(comp.name)
-    for (const prop in comp.props) {
-        if (Object.hasOwnProperty.call(comp.props, prop)) {
-            const val = comp.props[prop];
-            el.setAttribute(prop, val)
+    for (const prop in props) {
+        if (Object.hasOwnProperty.call(props, prop)) {
+            const val = props[prop];
+            if (/^on/.test(prop))
+                el.addEventListener(prop.slice(2).toLowerCase(), val)
+            else if (prop === 'style' && typeof val == 'object')
+                for (const p in prop) {
+                    if (Object.hasOwnProperty.call(prop, p)) {
+                        const v = prop[p];
+                        el.style[p] = v
+                    }
+                }
+            else
+                el.setAttribute(prop, val)
         }
     }
-    for (const child of comp.props.children) {
+    for (const child of children) {
         if (child[TYPE] == 0)
             render(child, el)
         else {
@@ -28,24 +39,6 @@ export function h(name, props, ...children) {
         [TYPE]: 0,
         name,
         props
-    }
-    if (typeof el == 'function') {
-        let comp = el(props, children)
-        comp.c()
-        return comp
-    }
-    let comp;
-    let carr = []
-    return {
-        [COMPONENT]: true,
-        c() {
-            if (props?.class === 'rrr') console.log(el)
-
-        },
-        m(target) {
-            carr.forEach(c => c.m(comp))
-            target.appendChild(comp)
-        }
     }
 }
 export function Fragment(...args) {
